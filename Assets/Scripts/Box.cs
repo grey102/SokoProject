2f92f27396fb762f5f535dddb8f75968540743e0
grey102 <60707551+grey102@users.noreply.github.com>
2020-05-23 15:07:16 +0300

    first commit

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public bool onCrox;
+
+    public bool Move(Vector2 direction)
+    {
+        if (BoxBlocked(transform.position, direction))
+        {
+            return false;
+        }
+        else
+        {
+            transform.Translate(direction);
+            TransformForCrox();
+            return true;
+        }
+    }
+
+    private void TransformForCrox()
+    { // описание закрытия "бомбы" 
+        GameObject[] bombs = GameObject.FindGameObjectsWithTag("Bomb");
+        foreach (var bomb in bombs)
+        {
+            if (transform.position.x == bomb.transform.position.x && transform.position.y == bomb.transform.position.y)
+            { // если координаты "коробки" и "бомбы" совпадают
+                GetComponent<SpriteRenderer>().color = Color.red; // меняем цвет "коробки" на красный
+                Debug.Log("Turn Color");
+                onCrox = true;
+                return;
+            }
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+        onCrox = false;
+    }
+
+    private bool BoxBlocked(Vector3 position, Vector2 direction)
+    {
+        Vector2 newPos = new Vector2(position.x, position.y) + direction; //  некий двумерный объект + направление
+        GameObject[] walls = GameObject.FindGameObjectsWithTag("Wall");
+        foreach (var wall in walls)
+        {
+            if (wall.transform.position.x == newPos.x && wall.transform.position.y == newPos.y)
+            {
+                return true;
+            }
+        }
+        GameObject[] boxes = GameObject.FindGameObjectsWithTag("Box");
+        foreach (var box in boxes)
+        {
+            if (box.transform.position.x == newPos.x && box.transform.position.y == newPos.y)
+            {
+                Box bx = box.GetComponent<Box>();
+                if (bx && bx.Move(direction))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
