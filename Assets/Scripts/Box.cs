032f3dae76db5a0eddc51b5b601b7390d0b37681
grey102 <60707551+grey102@users.noreply.github.com>
2020-05-29 13:03:25 +0300

    29-05-2020-13-05

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -1,26 +1,21 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Box : MonoBehaviour
 {
-
-
-    void Update()
+    private void Update()
     {
-
-        GameObject[] bombs = GameObject.FindGameObjectsWithTag("Bomb");
-        foreach (var bomb in bombs)
-        {
-            if (transform.position.x == bomb.transform.position.x && transform.position.y == bomb.transform.position.y)
-            { 
-                GetComponent<SpriteRenderer>().color = Color.red;
-                GetComponent<Rigidbody2D>().simulated = false;
-                return;
-            }
-        }
-
+       GameObject[] bombs = GameObject.FindGameObjectsWithTag("Bomb");
+       foreach (var bomb in bombs)
+       {
+          if (transform.position.x == bomb.transform.position.x && transform.position.y == bomb.transform.position.y)
+              {
+                 GetComponent<SpriteRenderer>().color = Color.red;
+                 return;
+              }
+       }
     }
+
 
 }
